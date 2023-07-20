4ce5f98c634347d6d4b8d31c15ae1326c7aa4f0a
Nikhil Pal <95010665+Nikhil7634@users.noreply.github.com>
2023-07-20 17:03:12 +0530

    done

---
+++ includes/footer.php
@@ -0,0 +1,119 @@
+<footer id="ft-footer" class="ft-footer-section">
+      <div class="container">
+        <div class="ft-footer-widget-wrapper">
+          <div class="row">
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 wow animated fadeInUp">
+              <div id="fastrans_about_company_v2-1" class="footer-widget ft-footer-widget widget_fastrans_about_company_v2">
+                <div class="pera-content">
+                  <div class="logo-widget">
+                    <div class="site-logo">
+                      <a href="index.php">
+                        <img src="http://themexriver.com/wp/fastrans/wp-content/uploads/2021/10/light-logo.png" alt="Awesome Image" />
+                      </a>
+                    </div>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <div class="ft-btn">
+                      <a class="d-flex justify-content-center align-items-center" href="contact.php" style="color: white">Get In Touch</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 wow animated fadeInUp">
+              <div id="nav_menu-1" class="footer-widget ft-footer-widget widget_nav_menu">
+                <h3 class="widget-title">Our Service</h3>
+                <div class="menu-footer-services-menu-container">
+                  <ul id="menu-footer-services-menu" class="menu">
+                    <li id="menu-item-863" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-863">
+                      <a href="services.php">Reliability &#038; Punctuality</a>
+                    </li>
+                    <li id="menu-item-865" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-865">
+                      <a href="services.php">Trusted Franchise</a>
+                    </li>
+                    <li id="menu-item-866" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-866">
+                      <a href="services.php">Warehoues Storage</a>
+                    </li>
+                    <li id="menu-item-862" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-862">
+                      <a href="services.php">Real Time Tracking</a>
+                    </li>
+                    <li id="menu-item-864" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-864">
+                      <a href="services.php">Transparent Pricing</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 wow animated fadeInUp">
+              <div id="nav_menu-3" class="footer-widget ft-footer-widget widget_nav_menu">
+                <h3 class="widget-title">Quick LInks</h3>
+                <div class="menu-footer-quick-container">
+                  <ul id="menu-footer-quick" class="menu">
+                    <li id="menu-item-1589" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-1589">
+                      <a href="index.php">Home</a>
+                    </li>
+                    <li id="menu-item-1590" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1590">
+                      <a href="about.php">About</a>
+                    </li>
+                    <li id="menu-item-1591" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1591">
+                      <a href="services.php">Our Services</a>
+                    </li>
+
+                    <li id="menu-item-1593" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1593">
+                      <a href="contact.php">Contact Us</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 wow animated fadeInUp">
+              <div id="fastrans_our_projects_v2-2" class="footer-widget ft-footer-widget widget_fastrans_our_projects_v2">
+                <!-- Instagram Widget -->
+                <div class="pera-content">
+                  <div class="gallery-widget clearfix">
+                    <h3 class="widget-title">Our Projects</h3>
+                    <ul class="zoom-gallery">
+                      <!-- Title -->
+                      <li class="image" style="
+                            background-image: url('https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/gl6.jpg');
+                          ">
+                        <a href="https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/gl6.jpg" data-source="https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/gl6.jpg"></a>
+                      </li>
+                      <li class="image" style="
+                            background-image: url('https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port3-1.jpg');
+                          ">
+                        <a href="https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port3-1.jpg" data-source="https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port3-1.jpg"></a>
+                      </li>
+                      <li class="image" style="
+                            background-image: url('https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port4-1.jpg');
+                          ">
+                        <a href="https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port4-1.jpg" data-source="https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port4-1.jpg"></a>
+                      </li>
+                      <li class="image" style="
+                            background-image: url('https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port3-1.jpg');
+                          ">
+                        <a href="https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port3-1.jpg" data-source="https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port3-1.jpg"></a>
+                      </li>
+                      <li class="image" style="
+                            background-image: url('https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port2-1.jpg');
+                          ">
+                        <a href="https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port2-1.jpg" data-source="https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port2-1.jpg"></a>
+                      </li>
+                      <li class="image" style="
+                            background-image: url('https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port1-1.jpg');
+                          ">
+                        <a href="https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port1-1.jpg" data-source="https://themexriver.com/fastrans-theme/fastrans/wp-content/uploads/sites/2/2021/10/port1-1.jpg"></a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="ft-footer-copywrite-1 text-center">
+          <span>Copyright @ 2021 Fastrans. All Rights Reserved</span>
+        </div>
+      </div>
+    </footer>
---
+++ includes/header.php
@@ -0,0 +1,136 @@
+<!-- Main Header -->
+<header class="ft11-main-header">
+      <!-- Header Top -->
+      <div class="ft11-header-top">
+        <div class="auto-container">
+          <div class="inner-container clearfix">
+            <!-- Top Left -->
+            <div class="ft11-top-left pull-right">
+              <ul class="top-list">
+                <li>
+                  <span class="icon"><i class="fa-solid fa-phone-volume"></i></span> Need any help? Call Us: +91 9823284760 | 9588419709
+                </li>
+                <li>
+                  <span class="icon"><i class="fa-solid fa-location-dot"></i></span> Infront of saravali ground,osspada, Saravali, Maharashtra 401501
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Header Upper -->
+      <div class="header-upper">
+        <div class="auto-container">
+          <div class="inner-container clearfix">
+            <!-- Logo -->
+            <div class="pull-left logo-box">
+              <div class="logo">
+                <a href="index.php">
+                  <img src="https://themexriver.com/fastrans-theme/fastrans/wp-content/themes/fastrans/assets/images/logo/logo-11.png" alt="Logo" title="" />
+                </a>
+              </div>
+            </div>
+            <div class="nav-outer clearfix">
+              <!-- Mobile Navigation Toggler -->
+              <div class="mobile-nav-toggler">
+                <span class="icon">
+                  <img src="https://themexriver.com/fastrans-theme/fastrans/wp-content/themes/fastrans/assets/images/icons/menu.png" alt="" />
+                </span>
+              </div>
+              <!-- Main Menu -->
+              <nav class="main-menu navbar-expand-md">
+                <div class="navbar-header">
+                  <!-- Toggle Button -->
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                </div>
+                <div class="navbar-collapse collapse clearfix" id="navbarSupportedContent">
+                  <ul class="navigation clearfix">
+                    <li id="menu-item-435" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-435">
+                      <a title="About" href="index.php" class="hvr-underline-from-left1" data-scroll data-options="easing: easeOutQuart">Home</a>
+                    </li>
+                    <li id="menu-item-435" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-435">
+                      <a title="About" href="about.php" class="hvr-underline-from-left1" data-scroll data-options="easing: easeOutQuart">About</a>
+                    </li>
+                    <li id="menu-item-435" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-435">
+                      <a title="About" href="services.php" class="hvr-underline-from-left1" data-scroll data-options="easing: easeOutQuart">Services</a>
+                    </li>
+                    <li id="menu-item-435" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-435">
+                      <a title="About" href="contact.php" class="hvr-underline-from-left1" data-scroll data-options="easing: easeOutQuart">Contact</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+              <!-- Main Menu End-->
+              <div class="outer-box clearfix">
+                <!-- Btn Box -->
+                <div class="btn-box">
+                  <a href="contact.php" class="theme-btn btn-style-one">
+                    Get a Quote
+                    <span class="icon fas"><i class="fa-solid fa-angles-right"></i></span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--End Header Upper-->
+      <!-- Sticky Header  -->
+      <div class="sticky-header">
+        <div class="auto-container clearfix">
+          <!--Logo-->
+          <div class="logo pull-left">
+            <a href="https://themexriver.com/fastrans-theme/fastrans">
+              <img src="https://themexriver.com/fastrans-theme/fastrans/wp-content/themes/fastrans/assets/images/logo/logo-11-mobile.png" alt="Logo" title="" />
+            </a>
+          </div>
+          <!--Right Col-->
+          <div class="pull-right">
+            <!-- Main Menu -->
+            <nav class="main-menu">
+              <!--Keep This Empty / Menu will come through Javascript-->
+            </nav>
+            <!-- Main Menu End-->
+            <!-- Main Menu End-->
+            <div class="outer-box clearfix">
+              <!-- Btn Box -->
+              <div class="btn-box">
+                <a href="" class="theme-btn btn-style-one">
+                  Get a Quote
+                  <span class="icon fas"><i class="fa-solid fa-angles-right"></i></span>
+                </a>
+              </div>
+              <!-- Mobile Navigation Toggler -->
+              <div class="mobile-nav-toggler">
+                <span class="icon">
+                  <img src="https://themexriver.com/fastrans-theme/fastrans/wp-content/themes/fastrans/assets/images/icons/menu.png" alt="" />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End Sticky Menu -->
+      <!-- Mobile Menu  -->
+      <div class="mobile-menu">
+        <div class="menu-backdrop"></div>
+        <div class="close-btn">
+          <span class="icon fas fa-times"></span>
+        </div>
+        <nav class="menu-box">
+          <div class="nav-logo">
+            <a href="https://themexriver.com/fastrans-theme/fastrans">
+              <img src="https://themexriver.com/fastrans-theme/fastrans/wp-content/themes/fastrans/assets/images/logo/logo-11-mobile.png" alt="Logo" title="" />
+            </a>
+          </div>
+          <div class="menu-outer">
+            <!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
+          </div>
+        </nav>
+      </div>
+      <!-- End Mobile Menu -->
+    </header>
